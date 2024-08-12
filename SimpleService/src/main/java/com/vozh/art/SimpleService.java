fe5fdd6ae13c75de48670eccd8820e03b042f2a0
Artem Vozhov <vozhov.artem1@gmail.com>
2024-08-12 15:34:29 +0300

    Simple service added

---
+++ SimpleService/src/main/java/com/vozh/art/SimpleService.java
@@ -0,0 +1,11 @@
+package com.vozh.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleService {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleService.class);
+    }
+}
