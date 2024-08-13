1a103e86c464faeda306a097a8201d9dfa18d864
Artem Vozhov <vozhov.artem1@gmail.com>
2024-08-13 13:04:03 +0300

    eureka server

---
+++ EurekaServer/src/main/java/com/vozh/art/eureka/EurekaApplication.java
@@ -0,0 +1,12 @@
+package com.vozh.art.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- EurekaServer/src/main/java/com/vozh/art/eureka/Main.java
+++
@@ -1,7 +0,0 @@
-package com.vozh.art.eureka;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
