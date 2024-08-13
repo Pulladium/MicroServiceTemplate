6423a38c3100a97825f7b55153384ce81f914e03
Artem Vozhov <vozhov.artem1@gmail.com>
2024-08-13 12:05:02 +0300

    service

--- ProcessingService/src/main/java/com/vozh/art/service/ProcessingService.java
+++ ProcessingService/src/main/java/com/vozh/art/service/ProcessingService.java
@@ -1,7 +1,18 @@
 package com.vozh.art.service;
 
+import com.vozh.art.dto.DataItemRequest;
+import com.vozh.art.webclient.DataServiceClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
 @Service
 public class ProcessingService {
+    private final DataServiceClient dataServiceClient;
+
+    public void processDataItem(DataItemRequest request) {
+        // Additional business logic or validations can be added here
+        dataServiceClient.addDataItem(request);
+    }
 }
