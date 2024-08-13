3455a1b7d7e6840feb9bbc426d7f7e36cf92af2a
Artem Vozhov <vozhov.artem1@gmail.com>
2024-08-13 13:11:59 +0300

    removed jpa, loadbalancer for feign

--- EurekaServer/src/main/java/com/vozh/art/eureka/EurekaApplication.java
+++ EurekaServer/src/main/java/com/vozh/art/eureka/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.vozh.art.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -7,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(EurekaApplication.class);
     }
 }
