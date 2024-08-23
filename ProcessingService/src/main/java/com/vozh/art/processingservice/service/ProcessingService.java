8242057b7a9c4b4395ee58072fb9032327634558
Artem Vozhov <vozhoart@fel.cvut.cz>
2024-08-23 11:26:10 +0300

    kafka listener configured as, gmail smtp server connection for NotificationService

--- ProcessingService/src/main/java/com/vozh/art/processingservice/service/ProcessingService.java
+++ ProcessingService/src/main/java/com/vozh/art/processingservice/service/ProcessingService.java
@@ -1,8 +1,10 @@
 package com.vozh.art.processingservice.service;
 
 import com.vozh.art.common.dto.DataItemRequest;
+import com.vozh.art.common.event.DataSentEvent;
 import com.vozh.art.processingservice.webclient.DataServiceClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 
@@ -10,9 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProcessingService {
     private final DataServiceClient dataServiceClient;
+    private final KafkaTemplate<String, DataSentEvent> dataSentEventKafkaTemplate;
 
     public void processDataItem(DataItemRequest request) {
         // Additional business logic or validations can be added here
-        dataServiceClient.addDataItem(request);
+//        dataServiceClient.addDataItem(request);
+
+        //Send message to kafka
+        DataSentEvent dataSentEvent = new DataSentEvent(request.name(), request.value());
+        dataSentEventKafkaTemplate.send("data-topic", dataSentEvent);
     }
 }
